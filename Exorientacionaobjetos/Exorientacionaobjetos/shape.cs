3a54fc333243e37a81d575b74d655b8639df79ab
jose luis yepes <yepes_jose@hotmail.com>
2018-11-05 20:07:56 +0100

    Shape

---
+++ Exorientacionaobjetos/Exorientacionaobjetos/shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exorientacionaobjetos
+{
+    public class shape
+    {
+        public virtual void Draw()
+
+
+            class Rectángulo : shape
+        {
+            public override void Draw()
+
+            {
+                Console.WriteLine("dibujar rectángulo");
+                base.Draw();
+            }
+        }
+    }
